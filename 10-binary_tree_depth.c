bb3453aab59e7de4774a219c3ae79933a9a8f188
Amr Adel <amr96.adel@gmail.com>
2024-01-04 00:24:27 +0200

    amr1

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Function that measures the depth of a binary tree
+ * @tree: tree to go through
+ * Return: the depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *node)
+{
+   if (node == NULL || node->parent == NULL)
+       return 0;
+   else
+       return 1 + binary_tree_depth((binary_tree_t *)node->parent);
+}
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,21 +1,27 @@
-#include"binary_trees.h"
+#include "binary_trees.h"
 
 /**
- * binary_tree_height - function that measures the height of a binary tree.
- * @tree: pointer to the root node of the tree to measure the height.
- * Return: Pointer to the created nodeL.
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int r, l;
+	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
-	r = binary_tree_height(tree->right);
-	l = binary_tree_height(tree->left);
-	if (l > r)
-		return (l + 1);
+	}
 	else
-		return (r + 1);
+	{
+		if (tree)
+		{
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((l > r) ? l : r);
+	}
 }
