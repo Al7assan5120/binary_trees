e34bab6c0cfd46375def57cfa8a1edcfdf9f7e1c
Amr Adel <amr96.adel@gmail.com>
2024-01-04 01:14:25 +0200

    amr 7

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,13 +10,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-		size_t left_leaves = binary_tree_leaves(tree->left);
-		size_t right_leaves = binary_tree_leaves(tree->right);
-		return (left_leaves + right_leaves);
-	}
+	size_t left_leaves = binary_tree_leaves(tree->left);
+	size_t right_leaves = binary_tree_leaves(tree->right);
+
+	return (left_leaves + right_leaves);
 }
